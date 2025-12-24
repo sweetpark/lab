01d5907a5bb23ce061d1985bc1c5ba918208b2b2
wypark <wypark@wiezon.com>
2025-12-24 12:02:37 +0900

    [FEAT] BATCH APPLICATION Project 추가

--- Application/src/main/java/com/example/application/AppApplication.java
+++ Application/src/main/java/com/example/application/AppApplication.java
@@ -5,15 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //통합실행 모듈
 @SpringBootApplication(scanBasePackages = {
-        "com.example.application",
-        "com.example.playground.aop",
-        "com.example.playground.cache",
-        "com.example.playground.crolling",
-        "com.example.playground.exception",
-//        "com.example.playground.exterior",
-        "com.example.playground.fileIO",
-        "com.example.playground.noti",
-        "com.example.playground.page"
+        "com.example.application"
+//        ,"com.example.playground.aop"
+//        ,"com.example.playground.cache"
+//        ,"com.example.playground.crolling"
+//        ,"com.example.playground.exception"
+//        ,"com.example.playground.exterior"
+//        ,"com.example.playground.fileIO"
+//        ,"com.example.playground.noti"
+//        ,"com.example.playground.page"
+        ,"com.example.playground.batch"
 })
 public class AppApplication {
 
