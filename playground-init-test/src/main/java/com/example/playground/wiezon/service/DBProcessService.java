fba9f47be07c7f21668f77cd0ce67decf3ceda6a
sweetpark <wooyeong1998@gmail.com>
2026-01-29 00:23:10 +0900

    [FEAT] json 파일 리더기, db 서비스 구현

--- playground-init-test/src/main/java/com/example/playground/wiezon/service/DBProcessService.java
+++ playground-init-test/src/main/java/com/example/playground/wiezon/service/DBProcessService.java
@@ -1,8 +1,14 @@
 package com.example.playground.wiezon.service;
 
+import com.example.playground.wiezon.dto.MetaData;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DBProcessService {
+
+
+    public void save(MetaData metaData){
+
+    }
 
 }
--- playground-init-test/src/main/java/com/example/playground/wiezon/service/FileReadService.java
+++ playground-init-test/src/main/java/com/example/playground/wiezon/service/FileReadService.java
@@ -1,33 +1,78 @@
 package com.example.playground.wiezon.service;
 
+import com.example.playground.wiezon.dto.MetaData;
+import com.example.playground.wiezon.util.EncUtil;
 import com.google.gson.Gson;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Service
 public class FileReadService {
 
 
-    public Map<String, Object> parseJson(Path path){
+    public MetaData parseJson(InputStream inputStream){
 
-        Map<String, Object> result = new HashMap<>();
+        MetaData metaData = null;
 
-        try(BufferedReader inputStream = new BufferedReader(new InputStreamReader(Files.newInputStream(path), "UTF-8"))) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             Gson gson = new Gson();
-            result = gson.fromJson(inputStream, Map.class);
-
-            System.out.println(result);
+            metaData = gson.fromJson(br, MetaData.class);
 
         } catch (IOException e) {
-            throw new RuntimeException("Failed to read json file : " + path, e);
+            throw new RuntimeException("Failed to read json file", e);
         }
 
 
-        return result;
+        return metaData;
+    }
+
+
+    public void dataPreProcess(MetaData metaData){
+        List<Map<String, Object>> rows = metaData.getRows();
+
+        for(Map<String, Object> row : rows){
+
+            for(String key : row.keySet()){
+
+                Map<String, Object> data = (Map<String, Object>) row.get(key);
+
+                if(data.get("crypto") != null){
+                    switch(data.get("crypto").toString()){
+                        case "ENC_HASH":
+                            row.put(key+"_ENC", Map.of("value", EncUtil.createEnc(data.get("value").toString())));
+                            row.put(key+"_HASH", Map.of("value", EncUtil.createHash(data.get("value").toString())));
+                            break;
+                        case "OTP":
+                            row.put(key, Map.of("value", EncUtil.createEncOtp()));
+                        case "PW":
+                            row.put(key, Map.of("value", EncUtil.createEncPw(data.get("value").toString())));
+                    }
+                }
+
+
+                // COLUMN : DATA
+                LocalDateTime now = LocalDateTime.now();
+                switch (data.get("value").toString()){
+                    case "CUR_YYMMDD":
+                        data.put("value", now.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+                        break;
+                    case "CUR_YYMMDDHHIISS":
+                        data.put("value", now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
