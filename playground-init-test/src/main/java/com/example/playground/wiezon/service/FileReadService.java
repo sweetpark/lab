5f50cc4bc991311445b6c10b3d7e8a011ca0d9d9
wypark <wypark@wiezon.com>
2026-01-29 14:40:17 +0900

    [FEAT] json file parse 구현

--- playground-init-test/src/main/java/com/example/playground/wiezon/service/FileReadService.java
+++ playground-init-test/src/main/java/com/example/playground/wiezon/service/FileReadService.java
@@ -1,78 +1,73 @@
 package com.example.playground.wiezon.service;
 
 import com.example.playground.wiezon.dto.MetaData;
+import com.example.playground.wiezon.util.CryptoType;
 import com.example.playground.wiezon.util.EncUtil;
 import com.google.gson.Gson;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Service
 public class FileReadService {
 
 
     public MetaData parseJson(InputStream inputStream){
-
-        MetaData metaData = null;
-
         try(BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             Gson gson = new Gson();
-            metaData = gson.fromJson(br, MetaData.class);
+            return gson.fromJson(br, MetaData.class);
 
         } catch (IOException e) {
             throw new RuntimeException("Failed to read json file", e);
         }
-
-
-        return metaData;
     }
 
-
     public void dataPreProcess(MetaData metaData){
-        List<Map<String, Object>> rows = metaData.getRows();
+        preprocessMetaData(metaData,LocalDateTime.now());
+    }
 
-        for(Map<String, Object> row : rows){
+    private void preprocessMetaData(MetaData metaData, LocalDateTime now) {
+        metaData.getRows().forEach(row -> preprocessRow(row, now));
+    }
+    private void preprocessRow(Map<String, Map<String, Object>> row, LocalDateTime now) {
+        Map<String, Map<String, Object>> additionalData = new HashMap<>();
 
-            for(String key : row.keySet()){
+        row.entrySet().forEach( entry -> {
+            chgEncValue(entry, additionalData);
+            chgDateValue(entry, now);
+        });
 
-                Map<String, Object> data = (Map<String, Object>) row.get(key);
-
-                if(data.get("crypto") != null){
-                    switch(data.get("crypto").toString()){
-                        case "ENC_HASH":
-                            row.put(key+"_ENC", Map.of("value", EncUtil.createEnc(data.get("value").toString())));
-                            row.put(key+"_HASH", Map.of("value", EncUtil.createHash(data.get("value").toString())));
-                            break;
-                        case "OTP":
-                            row.put(key, Map.of("value", EncUtil.createEncOtp()));
-                        case "PW":
-                            row.put(key, Map.of("value", EncUtil.createEncPw(data.get("value").toString())));
-                    }
+        row.putAll(additionalData);
+    }
+    private static void chgEncValue(Map.Entry<String, Map<String, Object>> entry, Map<String, Map<String, Object>> additionalData) {
+        if(entry.getValue().get("crypto") != null){
+            CryptoType cryptoType = CryptoType.from(entry.getValue().get("crypto"));
+            switch(cryptoType){
+                case CryptoType.ENC_HASH -> {
+                    additionalData.putIfAbsent(entry.getKey()+ "_ENC" , valueMap(EncUtil.createEnc(entry.getValue().get("value").toString())));
+                    additionalData.putIfAbsent(entry.getKey() + "_HASH" , valueMap(EncUtil.createHash(entry.getValue().get("value").toString())));
                 }
-
-
-                // COLUMN : DATA
-                LocalDateTime now = LocalDateTime.now();
-                switch (data.get("value").toString()){
-                    case "CUR_YYMMDD":
-                        data.put("value", now.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
-                        break;
-                    case "CUR_YYMMDDHHIISS":
-                        data.put("value", now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
-                        break;
-                    default:
-                        break;
-                }
+                case CryptoType.OTP -> entry.setValue(valueMap(EncUtil.createEncOtp()));
+                case CryptoType.PASSWORD -> entry.setValue(valueMap(EncUtil.createEnc(entry.getValue().get("value").toString())));
             }
         }
+    }
+    private static void chgDateValue(Map.Entry<String, Map<String, Object>> entry, LocalDateTime now) {
+        if(entry.getValue().get("value") != null){
+            switch (entry.getValue().get("value").toString()){
+                case "CUR_YYMMDD" -> entry.setValue(valueMap(now.format(DateTimeFormatter.ofPattern("yyyyMMdd"))));
+                case "CUR_YYMMDDHHIISS" -> entry.setValue(valueMap(now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))));
+            }
+        }
+    }
+    private static @NonNull Map<String, Object> valueMap(String now) {
+        Map<String, Object> newDate = new HashMap<>();
+        newDate.put("value", now);
+        return newDate;
     }
 }
