f162d07045cc0e1f5cd4b1f6ba9b1f1acea614fb
wypark <wypark@wiezon.com>
2026-02-04 17:10:31 +0900

    [FIX] 계약서 등록시, CO_NO 컬럼 인자 미매칭 추가 수정

--- playground-init-test/src/main/java/com/example/playground/wiezon/startegy/ContractProcessStrategy.java
+++ playground-init-test/src/main/java/com/example/playground/wiezon/startegy/ContractProcessStrategy.java
@@ -8,10 +8,6 @@ import com.example.playground.wiezon.service.FileReadService;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -22,13 +18,11 @@ import static com.example.playground.wiezon.util.CommonUtil.valueMap;
 public class ContractProcessStrategy implements MetaDataProcessStrategy{
 
     private final TableClassifier classifier;
-    private final DataSource dataSource;
     private final FileReadService fileReadService;
     private final DBProcessService dbProcessService;
 
-    public ContractProcessStrategy(TableClassifier classifier, DataSource dataSource, FileReadService fileReadService, DBProcessService dbProcessService) {
+    public ContractProcessStrategy(TableClassifier classifier, FileReadService fileReadService, DBProcessService dbProcessService) {
         this.classifier = classifier;
-        this.dataSource = dataSource;
         this.fileReadService = fileReadService;
         this.dbProcessService = dbProcessService;
     }
@@ -41,19 +35,17 @@ public class ContractProcessStrategy implements MetaDataProcessStrategy{
     @Override
     public void process(MetaData metaData, InitData propertiesData) {
 
-        String cono = getCoNo(dataSource);
-
-        if(cono == null){
-            throw new RuntimeException("등록될 영세 사업자번호가 존재하지 않습니다. 확인 부탁드립니다.");
-        }
-
-
         List<Map<String, Map<String, Object>>> newRows = metaData.getRows().stream().map(
                 templateRow -> {
                     Map<String, Map<String, Object>> deepRow = createNewRow(templateRow);
 
-                    if (isTemplateMatched(deepRow, "CO_NO", "${CO_NO}"))
-                        deepRow.put("CO_NO", valueMap(cono));
+                    if (isTemplateMatched(deepRow, "CO_NO", "${CO_NO}")){
+                        deepRow.put("CO_NO", valueMap(propertiesData.getMidList().getFirst().getCono()));
+                    }
+
+                    if(isTemplateMatched(deepRow, "ID", "${CO_NO}")){
+                        deepRow.put("ID", valueMap(propertiesData.getMidList().getFirst().getCono()));
+                    }
 
                     return deepRow;
                 }).toList();
@@ -67,32 +59,4 @@ public class ContractProcessStrategy implements MetaDataProcessStrategy{
     }
 
 
-    private static String getCoNo(DataSource dataSource) {
-        String sql =
-                "SELECT tmma.CO_NO " +
-                        "FROM TBSI_MS_MBS_ALL tmma " +
-                        "LEFT JOIN TBSI_CO tsc ON tmma.CO_NO = tsc.CO_NO " +
-                        "WHERE tmma.SM_MBS_CD = ? " +
-                        "AND tsc.CO_NO IS NULL " +
-                        "ORDER BY tmma.TO_DT DESC " +
-                        "LIMIT 1";
-
-        try(Connection connection = dataSource.getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(sql)) {
-
-            pstmt.setString(1, "A1");
-
-            try(ResultSet rs = pstmt.executeQuery()){
-                if(rs.next()){
-                    return rs.getString("CO_NO");
-                }else{
-                    throw new RuntimeException("영세를 가진 사업자번호가 존재하지 않습니다.");
-                }
-            }
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
